ef8221e71318f5eb9089af54d1bc793d80b1d1a4
Nicolas Bergeault <56303623+NicolasBergeault@users.noreply.github.com>
2021-09-22 16:29:31 +0200

    Ajout login

--- login_action.php
+++ login_action.php
@@ -1,4 +1,20 @@
 
 <?php
-require 'database.php';
+include 'database.php';
+
+$user = htmlspecialchars($_POST["user"]);
+$password = hash('sha256', htmlspecialchars($_POST["password"]));
+
+$query = "SELECT * from users WHERE user = :user";
+$req_pre = $cnx->prepare($query);
+$req_pre->bindParam(':user', $user, PDO::PARAM_STR);
+$req_pre->execute();
+
+$ligne=$req_pre->fetch(PDO::FETCH_OBJ);
+
+if ($ligne->password === $password) {
+    echo 'Bonjour '.$user.' !';
+} else {
+    echo 'Mauvaise information de connection';
+}
 ?>
