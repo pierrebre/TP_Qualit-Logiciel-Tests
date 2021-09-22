6aa002c0d4b80f4de9f3220f6f29b718b75c35d5
Nicolas Bergeault <56303623+NicolasBergeault@users.noreply.github.com>
2021-09-22 21:08:58 +0200

    Gérer le cas de user / email déjà dans la db

--- inscription_action.php
+++ inscription_action.php
@@ -13,17 +13,17 @@ include 'database.php';
     $password = hash('sha256', $_POST['password']);
     $password_verif = hash('sha256', $_POST['password_verif']);
 
-    $query = "INSERT INTO users (user, email, password) VALUES (:user, :email, :password)";
-    $req_pre = $cnx->prepare($query);
-    $req_pre->bindParam(':user', $user, PDO::PARAM_STR);
-    $req_pre->bindParam(':email', $email, PDO::PARAM_STR);
-    $req_pre->bindParam(':password', $password, PDO::PARAM_STR);
-    try {
-        $req_pre->execute($query);
+    $query = "INSERT INTO users (user, email, password) VALUES ('".$user."', '".$email."', '".$password."')";
+    if (!$cnx->exec($query)) {
+        $erreur = $cnx->errorInfo()[2];
+        if (strpos($erreur, "for key 'user'") !== false ) {
+            echo 'Username déjà existant, veuillez en choisir un autre.';
+        } else if (strpos($erreur, "for key 'email'") !== false ) {
+            echo 'Email déjà existant, veuillez en choisir un autre.';
+        } 
+    }
+    else {
         echo 'Entrée ajoutée dans la table';
-    } catch(Error $e){
-        $req_pre->rollBack();
-        echo "Erreur : " . $e->getMessage();
     }
  }
 
