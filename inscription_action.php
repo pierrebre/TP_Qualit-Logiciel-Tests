771c771bd1d2d4b3299f25fe2679eac52067434e
pierrebrbe <pierrepierre35150@gmail.com>
2021-09-22 16:01:57 +0200

    inscription

---
+++ inscription_action.php
@@ -0,0 +1,33 @@
+<?php
+
+include 'database.php';
+
+
+ // Vérifier si le formulaire est soumis 
+ if ( isset( $_POST['submit'] ) ) {
+    /* récupérer les données du formulaire en utilisant 
+       la valeur des attributs
+      */
+    $user = $_POST['user']; 
+    $email = $_POST['email']; 
+    $password = $_POST['password'];
+    $password_verif = $_POST['password_verif'];
+
+    
+
+    $sql = "INSERT INTO users (user, email, password)
+    VALUES ('$user', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+  
+    $conn->close();
+
+
+
+ }
+
+?>
