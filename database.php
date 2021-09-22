9ea032969979e85a9890a3c96adbb83a5056d421
Nicolas Bergeault <56303623+NicolasBergeault@users.noreply.github.com>
2021-09-22 14:16:49 +0200

    Modification connection db

--- database.php
+++ database.php
@@ -1,16 +1,17 @@
 <?php
 $ini = parse_ini_file('app.ini');
 
-$servername = $ini['db_name'];
+$serverhost = $ini['db_host'];
+$dbname = $ini['db_name'];
 $username = $ini['db_user'];
 $password = $ini['db_password'];
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($serverhost, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo 'ok';
+echo 'db connected';
 ?>
