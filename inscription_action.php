d503e88e60504bef1ae9631bffdcdd9bdffc063e
Nicolas Bergeault <56303623+NicolasBergeault@users.noreply.github.com>
2021-09-22 16:39:13 +0200

    Inscription bindparam + hash

--- inscription_action.php
+++ inscription_action.php
@@ -3,42 +3,29 @@
 include 'database.php';
 
 
- // Vérifier si le formulaire est soumis 
+ // Vérifier si le formulaire est soumis
  if ( isset( $_POST['submit'] ) ) {
-    /* récupérer les données du formulaire en utilisant 
+    /* récupérer les données du formulaire en utilisant
        la valeur des attributs
       */
-    $user = $_POST['user']; 
-    $email = $_POST['email']; 
+    $user = $_POST['user'];
+    $email = $_POST['email'];
     $password = $_POST['password'];
     $password_verif = $_POST['password_verif'];
 
-    
-
-    /*$sql = "INSERT INTO users (user, email, password)
-    VALUES ('$user', '$email', '$password')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+    $query = "INSERT INTO users (user, email, password) VALUES (:user, :email, :password)";
+    $req_pre = $cnx->prepare($query);
+    $req_pre->bindParam(':user', $user, PDO::PARAM_STR);
+    $req_pre->bindParam(':email', $email, PDO::PARAM_STR);
+    $req_pre->bindParam(':password', hash('sha256', $password), PDO::PARAM_STR);
+    $req_pre->execute();
+    try {
+        $cnx->exec($query);
+        echo 'Entrée ajoutée dans la table';
+    } catch(PDOException $e){
+        $cnx->rollBack();
+        echo "Erreur : " . $e->getMessage();
     }
-  
-    $conn->close();*/
-    $sql = "INSERT INTO users (user, email, password) VALUES ('$user', '$email', '$password')";
-
-        try{
-            
-            $cnx->exec($sql);
-            echo 'Entrée ajoutée dans la table';
-            }
-            catch(PDOException $e){
-                $cnx->rollBack();
-              echo "Erreur : " . $e->getMessage();
-            }
-
-
-
  }
 
 ?>
